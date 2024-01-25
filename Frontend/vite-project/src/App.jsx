c2724faf95872e0266d2d0dc7e30b43e9e15446c
sanjaykumardev <duderaj1976@gmail.com>
2024-01-25 14:11:18 +0530

    change

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import Register from './pages/register'
-import Login from "./pages/login"
+
+
 import Home from './pages/home'
 import {Route, Routes } from 'react-router-dom'
-import Formpage from  "./pages/formpage";
+
 function App() {
 
 
@@ -11,9 +10,6 @@ function App() {
     <>
     <Routes>
       <Route exact path="/"  element={<Home />}/>
-      <Route  path="/login"  element={<Login />}/>
-      <Route  path="/register" element={<Register/>} />
-      <Route  path="/formpage" element={<Formpage/>} />
 
    </Routes>
     </>
